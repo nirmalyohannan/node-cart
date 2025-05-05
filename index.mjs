815a6412d92312a4f2192e539d176752be2e9a26
Nirmal Yohannan <nirmalyohannan.in@gmail.com>
2025-05-06 01:09:45 +0530

    Connected to MongoDB Atlas

--- index.mjs
+++ index.mjs
@@ -1,12 +1,30 @@
-import { express, json } from 'express';
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+// Load environment variables from .env file
+// Which is accessed via process.env
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+    .then(() => console.log('✅Connected to MongoDB'))
+    .catch((err) => console.log("❌", err));
+
+
 const app = express();
 
-app.use(json());
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.status(200).send({ success: true, message: 'Welcome to the NodeCart API!' });
 });
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.listen(PORT, () => {
+    console.log('Server is running on port ', PORT);
 });
